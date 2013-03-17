508033eacc64bc41d1c016a1aee4408db90decd2
Jesse Squires <jesse.d.squires@gmail.com>
2013-03-17 14:28:58 -0400

    Initial Commit

---
+++ FlatButtonDemo/JSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSAppDelegate.h
+//  FlatButtonDemo
+//
+//  Created by Jesse Squires on 3/17/13.
+//  Copyright (c) 2013 Hexed Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlatButtonDemo/JSViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSViewController.h
+//  FlatButtonDemo
+//
+//  Created by Jesse Squires on 3/17/13.
+//  Copyright (c) 2013 Hexed Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSViewController : UIViewController
+
+@end
