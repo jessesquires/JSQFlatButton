5cb66e7b85eb073eee6d6a26c8422e456cae0df6
Jesse Squires <jesse.d.squires@gmail.com>
2013-03-17 18:14:03 -0400

    demo color tweaks

--- FlatButtonDemo/DemoViewController.m
+++ FlatButtonDemo/DemoViewController.m
@@ -19,21 +19,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor darkGrayColor];
     
-    self.button1.buttonBackgroundColor = [UIColor colorWithHue:0.0f saturation:0.0f brightness:0.60f alpha:1.0f];
+    self.button1.buttonBackgroundColor = [UIColor colorWithRed:0.32f green:0.64f blue:0.32f alpha:1.00f]; //[UIColor colorWithHue:0.0f saturation:0.0f brightness:0.60f alpha:1.0f];
     self.button1.buttonForegroundColor = [UIColor colorWithHue:0.0f saturation:0.0f brightness:1.0f alpha:1.0f];
     self.button1.titleLabel.font = [UIFont boldSystemFontOfSize:18.0f];
     [self.button1 setFlatTitle:@"  Press Me"];
-    [self.button1 setFlatImage:[UIImage imageNamed:@"arrow_right"]];
+    [self.button1 setFlatImage:[UIImage imageNamed:@"arrow_up"]];
     
-    self.button2.buttonBackgroundColor = [UIColor colorWithRed:255.0f/255.0f green:246.0f/255.0f blue:150.0f/255.0f alpha:1.0f];
-    self.button2.buttonForegroundColor = [UIColor colorWithRed:106.0f/255.0f green:183.0f/255.0f blue:197.0f/255.0f alpha:1.0f];
+    self.button2.buttonBackgroundColor = [UIColor colorWithHue:0.0f saturation:0.0f brightness:1.0f alpha:1.0f]; //[UIColor colorWithRed:255.0f/255.0f green:246.0f/255.0f blue:150.0f/255.0f alpha:1.0f];
+    self.button2.buttonForegroundColor = [UIColor colorWithRed:0.18f green:0.59f blue:0.71f alpha:1.00f]; //[UIColor colorWithRed:106.0f/255.0f green:183.0f/255.0f blue:197.0f/255.0f alpha:1.0f];
     self.button2.titleLabel.font = [UIFont boldSystemFontOfSize:26.0f];
-    [self.button2 setFlatTitle:@"  Play"];
-    [self.button2 setFlatImage:[UIImage imageNamed:@"play"]];
+    [self.button2 setFlatTitle:@"  Tap Tap"];
+    [self.button2 setFlatImage:[UIImage imageNamed:@"graph_bar"]];
     
-    self.button3.buttonBackgroundColor = [UIColor colorWithRed:231.0f/255.0f green:110.0f/255.0f blue:121.0f/255.0f alpha:1.0f];
-    self.button3.buttonForegroundColor = [UIColor colorWithHue:0.0f saturation:0.0f brightness:1.0f alpha:1.0f];
+    self.button3.buttonBackgroundColor = [UIColor colorWithRed:0.74f green:0.21f blue:0.18f alpha:1.00f]; //[UIColor colorWithRed:231.0f/255.0f green:110.0f/255.0f blue:121.0f/255.0f alpha:1.0f];
+    self.button3.buttonForegroundColor = [UIColor colorWithRed:255.0f/255.0f green:246.0f/255.0f blue:150.0f/255.0f alpha:1.0f]; //[UIColor colorWithHue:0.0f saturation:0.0f brightness:1.0f alpha:1.0f];
     self.button3.titleLabel.font = [UIFont fontWithName:@"Arial" size:34.0f];
     self.button3.titleLabel.numberOfLines = 2;
     self.button3.titleLabel.textAlignment = NSTextAlignmentCenter;
@@ -44,12 +45,12 @@
     self.button4.shouldHighlightImage = YES;
     self.button4.buttonForegroundColor = [UIColor colorWithRed:244.0f/255.0f green:153.0f/255.0f blue:108.0f/255.0f alpha:1.0f];
     [self.button4 setFlatTitle:nil];
-    [self.button4 setFlatImage:[UIImage imageNamed:@"plus"]];
+    [self.button4 setFlatImage:[UIImage imageNamed:@"shuffle"]];
     
     self.button5.buttonBackgroundColor = [UIColor colorWithRed:244.0f/255.0f green:153.0f/255.0f blue:108.0f/255.0f alpha:1.0f];
     self.button5.buttonForegroundColor = [UIColor colorWithHue:0.0f saturation:0.0f brightness:1.0f alpha:1.0f];
     [self.button5 setFlatTitle:nil];
-    [self.button5 setFlatImage:[UIImage imageNamed:@"plus"]];
+    [self.button5 setFlatImage:[UIImage imageNamed:@"record"]];
     
     self.button6.buttonBackgroundColor = nil;
     self.button6.shouldHighlightText = YES;
